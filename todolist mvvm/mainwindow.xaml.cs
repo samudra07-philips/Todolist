895e46067912c3c42ce344bec7d4619b708ef717
Barma <Samudragupta.Barma@philips.com>
2025-06-11 02:14:27 +0530

    Initial commit of the solution

---
+++ todolist mvvm/mainwindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using todolist_mvvm.view;
+
+namespace todolist_mvvm
+{
+    /// <summary>
+    /// Interaction logic for mainwindow.xaml
+    /// </summary>
+    public partial class Mainwindow : Window
+    {
+        public Mainwindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new LoginPage());
+        }
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.CanGoBack) { MainFrame.GoBack(); }
+        }
+    }
+}
---
+++ todolist mvvm/view/loginpage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace todolist_mvvm.view
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void textboxsample_TextChanged(object sender, TextChangedEventArgs e)
+        {
+         
+        }
+
+        private void passboxsample_TextChanged(object sender, RoutedEventArgs e)
+        {
+          
+        }
+
+        private void Signedup(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void Logedin(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Todo());
+        }
+        private void RemoveText(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text == "Enter your name" || textBox.Text == "Enter your password")
+            {
+                textBox.Text = "";
+            }
+        }
+        private void AddText(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                if (textBox.Name == "textboxsample")
+                {
+                    textBox.Text = "Enter your name";
+                }
+                else if (textBox.Name == "passboxsample")
+                {
+                    textBox.Text = "Enter your password";
+                }
+            }
+        }
+    }
+}
