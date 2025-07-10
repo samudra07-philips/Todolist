970b598b28458af6b8524997d45a3850492cbb89
Barma <Samudragupta.Barma@philips.com>
2025-07-10 10:51:45 +0530

    unity

--- Todolist.Services/TaskDto.cs
+++ Todolist.Services/TaskDto.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
-
+using todolist_mvvm.Data;
+using todolist_mvvm.model;
 namespace Todolist.Services
 {
     public class TaskDto
@@ -12,9 +13,11 @@ namespace Todolist.Services
         [DataMember] public int Id { get; set; }
         [DataMember] public string Name { get; set; }
         [DataMember] public string Description { get; set; }
-        [DataMember] public TaskPriority Priority { get; set; }
+        [DataMember] public  TaskPriority taskPriority { get; set; }
         [DataMember] public bool IsCompleted { get; set; }
         [DataMember] public DateTime? CompletedAt { get; set; }
         [DataMember] public int UserId { get; set; }
+
+        
     }
 }
