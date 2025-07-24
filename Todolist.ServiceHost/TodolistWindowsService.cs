d3ad84d5c35b9c2751c91f8b8d1873e3fb3735b9
Barma, Samudragupta <Samudragupta.Barma@philips.com>
2025-07-24 11:54:58 +0530

    bug

--- Todolist.ServiceHost/TodolistWindowsService.cs
+++ Todolist.ServiceHost/TodolistWindowsService.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;            
 using System.ServiceModel;
+using System.ServiceModel.Description;
 using System.ServiceProcess;
-using Unity;
-using Unity.Wcf;
 using Todolist.Services;
 using Todolist.Services.Data;          
-using System.Data.Entity;            
+using Unity;
+using Unity.Wcf;
 
 namespace Todolist.ServiceHost
 {
@@ -18,27 +20,70 @@ namespace Todolist.ServiceHost
         {
             // 1) Apply EF Migrations at startup
             Database.SetInitializer(
-              new MigrateDatabaseToLatestVersion<AppDbContext,
+              new MigrateDatabaseToLatestVersion<
+                  AppDbContext,
                   Todolist.Service.Migrations.Configuration>());
             using (var ctx = new AppDbContext())
                 ctx.Database.Initialize(force: true);
 
-            // 2) Build Unity container and register types
+            // 2) Build Unity container
             var container = new UnityContainer();
             UnityConfig.RegisterComponents(container);
 
-            // 3) Self‑host the WCF services
-            var userHost = new UnityServiceHost(container, typeof(UserService), new Uri("http://localhost:8000/UserService"));
-            userHost.AddServiceEndpoint(typeof(IUserService), new BasicHttpBinding(), "");
+            // For UserService
+            var userBase = new Uri("http://localhost:8000/UserService");
+            var userHost = new UnityServiceHost(container, typeof(UserService), userBase);
+
+            // Add metadata behavior
+            var smb1 = new ServiceMetadataBehavior
+            {
+                HttpGetEnabled = true,
+                HttpGetUrl = userBase
+            };
+            userHost.Description.Behaviors.Add(smb1);
+
+            // Add service endpoint
+            userHost.AddServiceEndpoint(typeof(IUserService),
+                                        new BasicHttpBinding(),
+                                        "");
+
+            // Add MEX endpoint
+            userHost.AddServiceEndpoint(typeof(IMetadataExchange),
+                                        MetadataExchangeBindings.CreateMexHttpBinding(),
+                                        "mex");
+
             userHost.Open();
             _hosts.Add(userHost);
 
-            var taskHost = new UnityServiceHost(container, typeof(TaskService), new Uri("http://localhost:8001/TaskService"));
-            taskHost.AddServiceEndpoint(typeof(ITaskService), new BasicHttpBinding(), "");
+            // For TaskService
+            var taskBase = new Uri("http://localhost:8001/TaskService");
+            var taskHost = new UnityServiceHost(container, typeof(TaskService), taskBase);
+
+            // Add metadata behavior
+            var smb2 = new ServiceMetadataBehavior
+            {
+                HttpGetEnabled = true,
+                HttpGetUrl = taskBase
+            };
+            taskHost.Description.Behaviors.Add(smb2);
+
+            // Add service endpoint
+            taskHost.AddServiceEndpoint(typeof(ITaskService),
+                                        new BasicHttpBinding(),
+                                        "");
+
+            // Add MEX endpoint
+            taskHost.AddServiceEndpoint(typeof(IMetadataExchange),
+                                        MetadataExchangeBindings.CreateMexHttpBinding(),
+                                        "mex");
+
             taskHost.Open();
             _hosts.Add(taskHost);
 
+
         }
+
+
 
         protected override void OnStop()
         {
@@ -60,3 +105,7 @@ namespace Todolist.ServiceHost
         }
     }
 }
+
+
+
+    
