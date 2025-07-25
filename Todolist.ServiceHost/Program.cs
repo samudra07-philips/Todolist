1f929e144ae26009f2ca1956d5f313c5596dacff
Barma, Samudragupta <Samudragupta.Barma@philips.com>
2025-07-25 10:47:44 +0530

    a

--- Todolist.ServiceHost/Program.cs
+++ Todolist.ServiceHost/Program.cs
@@ -1,24 +1,16 @@
 ﻿using System;
 using System.ServiceProcess;
+using Todolist.ServiceHost;
 
-namespace Todolist.ServiceHost
+static class Program
 {
-    static class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            var service = new TodolistWindowsService();
+        var service = new TodolistWindowsService();
 
-            if (Environment.UserInteractive)
-            {
-              
-                service.RunAsConsole();
-            }
-            else
-            {
-               
-                ServiceBase.Run(service);
-            }
-        }
+        if (Environment.UserInteractive)
+            service.RunAsConsole();
+        else
+            ServiceBase.Run(service);
     }
 }
